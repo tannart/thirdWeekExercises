a02d3903745ab533a31327b19f6eed4eaae49a8b
charliembrodie <cbrodi01@cspc060.dcs.bbk.ac.uk>
2012-10-15 15:33:06 +0100

    See Groovy commit.

---
+++ ArrayCopier.java
@@ -0,0 +1,16 @@
+public class ArrayCopier{
+	int[] src = new int[];
+	int[] dist = new dist[];
+	
+	public void MakeArray(int i){
+	    for(int a =0; a <i.length-1; a++){
+	        i.charAt(a) = src[a];
+		}
+	}
+	
+	public void copy(src[], dist[]){
+	    for(int a = 0;a < src[].length-1; a++){
+		    dist[a] = src[a];
+		}
+	}
+}
