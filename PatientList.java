6cca261c80c1b69455c1dda0ca3ade77c78de4b7
charliembrodie <cbrodi01@cspc051.dcs.bbk.ac.uk>
2012-10-18 10:22:26 +0100

    I can't believe I actually managed to do it, just goes to show what a bit of elbow grease and much needed rest can do for you, exercise complete

--- PatientList.java
+++ PatientList.java
@@ -3,7 +3,7 @@ public class PatientList{
 	
 	public static void main(String[] args){
 	    Patient patientListBegin;
-		
+		Patient patientListEnd;
 	
 	    Patient firstPatient = new Patient("John", 33, "Tuberculosis");
 		Patient secondPatient = new Patient("Derek", 45,"Consumption");
@@ -15,8 +15,10 @@ public class PatientList{
 		Patient eighthPatient = new Patient("Susan", 35,"Herpes");
 		Patient ninthPatient = new Patient("Roger", 77, "Violetta");
 		Patient tenthPatient = new Patient("Betty", 53,"Turpett");
+		Patient eleventhPatient = new Patient("Joffrey", 17, "ArseHole");
 		
 		patientListBegin = firstPatient;
+		patientListEnd = eleventhPatient;
 		patientListBegin.addPatient(secondPatient);
 		patientListBegin.addPatient(thirdPatient);
 		patientListBegin.addPatient(fourthPatient);
@@ -26,8 +28,22 @@ public class PatientList{
 		patientListBegin.addPatient(eighthPatient);
 		patientListBegin.addPatient(ninthPatient);
 		patientListBegin.addPatient(tenthPatient);
+		patientListBegin.addPatient(eleventhPatient);
+		
+		patientListBegin.deletePatient(sixthPatient);
+		patientListBegin.deletePatient(twelfthPatient);
+		
+		
+		
 		
 		for(Patient pos = patientListBegin; pos.getNext()!= null; pos = pos.getNext()){
+		    System.out.println(pos.getName());
+			System.out.println(pos.getIllness());
+			System.out.println(pos.getAge());
+			System.out.println("");
+		}
+		
+		for(Patient pos = patientListEnd; pos.getPrev()!= null; pos = pos.getPrev()){
 		    System.out.println(pos.getName());
 			System.out.println(pos.getIllness());
 			System.out.println(pos.getAge());
