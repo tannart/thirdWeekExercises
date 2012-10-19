e32276800614bb28fe0899f6796032029c554827
Girls Charlotte Visitors <charlotte@charlotte-lanes-imac.local>
2012-10-19 15:02:58 +0100

    thi

--- UtilitiesList.java
+++ UtilitiesList.java
@@ -1,8 +1,11 @@
-public class ListUtilities{
+public class UtilitiesList{
 
-    public ListUtitlies(){
+    public UtilitiesList(){
     }
     
-    public static arrayToList(int[] theArray){ //method converting the input array to a list.
+    public static IntList arrayToList(int[] theArray){ //method converting the input array to a list.
         for(int i = 1; i < theArray.length; i++){
-             theArray[i]
+            beginIntList.AddInt(theArray[i]);
+        }
+    }
+}
