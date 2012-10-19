9818c8c7dfa3b76cc11701ec7959ccbc9c8e27cc
Girls Charlotte Visitors <charlotte@charlotte-lanes-imac.local>
2012-10-19 15:02:23 +0100

    I hope this works

--- IntList.java
+++ IntList.java
@@ -1,16 +1,17 @@
 public class IntList{
     private int theInt;
+    private IntList next, previous;
 
-    public Intlist(int theInt){
+    public IntList(int theInt){
         this.theInt = theInt;
         this.next = null;
         this.previous = null;
     }
     
-    public addInt(int newInt){
+    public void addInt(IntList newInt){
         if(this.next == null){
             this.next = newInt;
-            newInt.prev = this;
+            newInt.previous = this;
         } else {
             this.next.addInt(newInt);
         }
