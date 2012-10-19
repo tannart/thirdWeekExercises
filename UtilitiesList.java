9bef62f8abf4dbd2243a55b788ecc3506bc11e9c
Girls Charlotte Visitors <charlotte@charlotte-lanes-imac.local>
2012-10-19 13:59:55 +0100

    I'm feeling a little apprehensive about the suggestion that I may have to use this over multiple exercises though not enormously worried

---
+++ UtilitiesList.java
@@ -0,0 +1,8 @@
+public class ListUtilities{
+
+    public ListUtitlies(){
+    }
+    
+    public static arrayToList(int[] theArray){ //method converting the input array to a list.
+        for(int i = 1; i < theArray.length; i++){
+             theArray[i]
