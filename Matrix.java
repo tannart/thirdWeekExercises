c0a154e1d2d09f46c4b432cf216dc3be46abf20d
Charlie Brodie <charliebrodie@Charlie-Brodies-MacBook-Pro.local>
2012-11-23 14:38:11 +0000

    I have now completed this exercise, full functionality.

--- Matrix.java
+++ Matrix.java
@@ -1,20 +1,111 @@
 public class Matrix{
-    int[][] matArray;
-	int column;
 	
-	Matrix(int row, int column){
-	    matArray = new int[row][column];
-		for(int i = 0; i < row; i++){
-		    for(int j = 0; j< column; j++){
-		    matArray[i][j] = 1;
+	private int[][] matrix;
+
+
+	public Matrix(int a, int b){
+		this.matrix = new int[a][b];
+
+		for(int i = 0; i < a; i++){
+			for(int e = 0; e < b; e++){
+
+				matrix[i][e] = 1;
 			}
 		}
 	}
-	int setElement(int row, int column, int value){
-	    matArray[row][column] = value;	
-		return column;
+
+	public void setElement(int a, int b, int set){
+		if(a < matrix.length && b < matrix[0].length){
+			matrix[a][b] = set;
+			System.out.println("The element " + set + " has been added to the matrix");
+
+		} else {
+
+			System.out.println("That location isnt in the matrix.");
+		}
 	}
-	void setRow(int row, String values){
-	    for(int i = 0; i = row.length-1; i++){
-		    
+
+	public void setRow(int a, String s){
+		if(a < matrix.length && s.length() == matrix[0].length){
+
+			for(int i = 0; i < matrix[0].length; i++){
+				System.out.println("This is working");
+
+				matrix[a][i] = Character.getNumericValue(s.charAt(i));
+			}
+
+			System.out.println("Row number " + a + " has now been changed to " + s);
+
+		} else {
+
+			System.out.println("That location isnt in the matrix or the string is the wrong size, please try again");
+		}
+	}
+
+	public void setColumn(int a, String s){
+		if(a<matrix[0].length && s.length() == matrix.length){
+
+			for(int i = 0; i < matrix.length; i++){
+
+
+				matrix[i][a] = Character.getNumericValue(s.charAt(i));
+			}
+
+			System.out.println("Column number " + a + " has now been changed to " + s);
+
+		} else {
+
+			System.out.println("That location isnt in the matrix or the string is the wrong size, please try again");
+		}
+	}
+
+	public String toString(){
+		String output = "[";
+
+		for(int a = 0; a < matrix.length; a++){
+			for(int i = 0; i < matrix[0].length; i++){
+				output += matrix[a][i];
+				if(i!=matrix[0].length-1){
+					output += ",";
+				}
+			}
+
+			if(a!=matrix[0].length-1){
+				output += ";";				
+			}
+		}
+
+		output += "]";
+		return output;
+	}
+
+	public String prettyPrint(){
+
+		String output = "";
+
+		for(int a = 0; a < matrix.length; a++){
+			output += "Line " + a + ": ";
+
+			for(int i = 0; i < matrix[0].length; i++){
+
+				output += matrix[a][i];
+				if(i!=matrix[0].length-1){
+
+					output += " ,";
+				}
+			}
+
+			if(a!=matrix[0].length-1){
+
+				output += ".   ";				
+			}
+		}
+
+		return output;
+	}
 }
+
+
+
+
+
